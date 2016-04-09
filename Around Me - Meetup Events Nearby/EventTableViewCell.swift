571b4ab05f9ad311ba2cb9eae5d3b4f9f2c608ec
Evan Lewis <evanlws12@gmail.com>
2016-04-09 15:13:43 -0400

    Added custom table view cells

---
+++ Around Me - Meetup Events Nearby/EventTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  EventTableViewCell.swift
+//  Around Me - Meetup Events Nearby
+//
+//  Created by Evan Lewis on 4/9/16.
+//  Copyright © 2016 Evan Lewis. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var locationLabel: UILabel!
+  @IBOutlet weak var timeLabel: UILabel!
+  @IBOutlet weak var innerView: UIView!
+ 
+  override func awakeFromNib() {
+    innerView.layer.shadowColor = UIColor.blackColor().CGColor;
+    innerView.layer.shadowOffset = CGSizeMake(1, 2);
+    innerView.layer.shadowOpacity = 0.4;
+    innerView.layer.shadowRadius = 1.0;
+  }
+  
+  override func setHighlighted(highlighted: Bool, animated: Bool) {
+    if highlighted {
+      innerView.layer.shadowColor = UIColor.clearColor().CGColor;
+    } else {
+      innerView.layer.shadowColor = UIColor.blackColor().CGColor;
+    }
+  }
+  
+  override func setSelected(selected: Bool, animated: Bool) {
+    if selected {
+      innerView.layer.shadowColor = UIColor.clearColor().CGColor;
+    } else {
+      innerView.layer.shadowColor = UIColor.blackColor().CGColor;
+    }
+  }
+}
